304f36f01ad35e0b9f01cfece1123c8748ebf598
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2023-10-13 10:24:25 +0200

    deleted template files and added example controller

---
+++ WebAPI/WebAPI/Controllers/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorld : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> GetHelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
