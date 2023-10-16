db3b6ba4702a8d513587981da715edd094dca33e
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2023-10-16 09:47:33 +0200

    created all product related files to demonstrate file structure

---
+++ WebAPI/WebAPI/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Data;
+
+public class DataContext : IdentityDbContext<IdentityUser>
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+}
