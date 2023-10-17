17d4254368041363f4fdb2517bfc3c15ca3b20f5
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2023-10-17 11:19:45 +0200

    Create generic and product repos

---
+++ WebAPI/WebAPI/Helpers/Repositories/GenericRepo.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using WebAPI.Data;
+
+namespace WebAPI.Helpers.Repositories;
+
+public abstract class GenericRepo<T> where T : class
+{
+    private readonly DataContext _context;
+
+    protected GenericRepo(DataContext context)
+    {
+        _context = context;
+    }
+
+    public virtual async Task<T?> CreateAsync(T entity)
+    {
+        _context.Set<T>().Add(entity);
+        await _context.SaveChangesAsync();
+
+        return entity;
+    }
+
+    public virtual async Task<T?> GetAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _context.Set<T>().FirstOrDefaultAsync(predicate);
+    }
+
+    public virtual async Task<List<T>> GetAllAsync()
+    {
+        return await _context.Set<T>().ToListAsync();
+    }
+
+    public virtual async Task<List<T>> GetAllAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _context.Set<T>().Where(predicate).ToListAsync();
+    }
+
+    public virtual async Task<T?> UpdateAsync(T entity)
+    {
+        _context.Set<T>().Update(entity);
+        await _context.SaveChangesAsync();
+
+        return entity;
+    }
+
+    public virtual async Task<bool> DeleteAsync(T entity)
+    {
+        try
+        {
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+        catch { return false; }
+    }
+}
