0e5b34586b03ca8adb6e1dc7453c4e9123294de4
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2023-10-18 10:23:35 +0200

    Create new test logic to work with new service method GetAllFilteredAsync

--- WebAPI/WebAPITest/ProductServiceTests.cs
+++ WebAPI/WebAPITest/ProductServiceTests.cs
@@ -1,6 +1,8 @@
+using System.Linq.Expressions;
 using WebAPI.Data;
 using WebAPI.Helpers.Repositories;
 using WebAPI.Helpers.Services;
+using WebAPI.Models.Entities;
 
 namespace WebAPITest;
 
@@ -29,14 +31,31 @@ public class ProductServiceTests
     }
 
     [Theory]
-    [InlineData("Pants", 2)]
-    [InlineData("pAnTS", 2)]
-    [InlineData("Shirts", 2)]
-    [InlineData("Shoes", 0)]
-    [InlineData("MadeUpCategory", 0)]
-    public async Task GetAllByCategoryAsync_ShouldReturnAllRelevantProducts(string categoryName, int expectedAmount)
+    // Test only categories
+    [InlineData("Pants", null, 2)]
+    [InlineData("Shoes", "", 0)]
+    [InlineData("MadeUpCategory", "", 0)]
+    // Test only tags
+    [InlineData("", "Kids", 1)]
+    [InlineData(null, "Unisex", 1)]
+    [InlineData(null, "Featured", 4)]
+    // Categories and tags
+    [InlineData("Shirts", "Kids", 0)]
+    [InlineData("Pants", "Unisex", 1)]
+    [InlineData("Shirts", "Sport", 1)]
+    public async Task GetAllFilteredAsync_ShouldReturnAllRelevantProducts(string categoryName, string tagName, int expectedAmount)
     {
-        var result = await _productService.GetAllByCategoryAsync(categoryName);
+        List<Expression<Func<ProductEntity, bool>>> filters = new();
+
+        if (!string.IsNullOrWhiteSpace(tagName))
+            filters.Add(x =>
+                x.Tags.Any(tag => tag.Name.ToLower() == tagName.ToLower()));
+
+        if (!string.IsNullOrWhiteSpace(categoryName))
+            filters.Add(x =>
+                x.Category.Name.ToLower() == categoryName.ToLower());
+
+        var result = await _productService.GetAllFilteredAsync(filters);
 
         Assert.Equal(expectedAmount, result.Count);
     }
