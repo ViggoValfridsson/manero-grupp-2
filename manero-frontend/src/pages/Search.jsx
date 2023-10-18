dbb2443dd781403a5092bec0ae94d8819d33621b
Jimmieost <jimmie.ost@utb.ecutbildning.se>
2023-10-18 11:57:55 +0200

    Add category cards

--- manero-frontend/src/pages/Search.jsx
+++ manero-frontend/src/pages/Search.jsx
@@ -1,8 +1,17 @@
+import SearchPageCategoryCard from "../components/SearchPageCategoryCard";
 import { apiDomain } from "../helpers/api-domain";
 import useFetch from "../hooks/useFetch";
 
 export default function Search() {
-  const categories = useFetch(`${apiDomain.https}/api/category`);
-  console.log(categories?.data);
-  return <div>Search</div>;
+  const categories = useFetch(`${apiDomain.https}/api/categories`);
+  return (
+    <div className="search-page">
+      <div>Tags</div>
+      <div className="category-cards-container">
+        {categories?.data?.map((category) => (
+          <SearchPageCategoryCard category={category} />
+        ))}
+      </div>
+    </div>
+  );
 }
