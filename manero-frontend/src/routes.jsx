5df1fd54a52196d103cd3ff66c2eb6700dd27980
simonoff <hellosimonoff@gmail.com>
2023-10-19 10:00:32 +0200

    Add cart page
    Co-authored-by: Leon Lagergren <Lwonla@users.noreply.github.com>

--- manero-frontend/src/routes.jsx
+++ manero-frontend/src/routes.jsx
@@ -1,9 +1,11 @@
+import Cart from "./pages/Cart";
 import Home from "./pages/Home";
 import Search from "./pages/Search";
 
 const routes = [
   { path: "/", element: <Home /> },
   { path: "/search", element: <Search /> },
+  { path: "/cart", element: <Cart /> },
   { path: "*", element: <h1>Not found</h1> },
 ];
 
