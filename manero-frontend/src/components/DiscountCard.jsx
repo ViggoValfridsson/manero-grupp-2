2f48059519a0886409229d79b65eb0fa7de6af95
fenetreVRT <fenetreverte@outlook.com>
2023-10-19 15:45:05 +0200

    add discount card component
    Co-authored-by: Natanael Augustin
    Co-authored-by:
    Co-authored-by: Simon <simon-off@users.noreply.github.com>
    Co-authored-by: Leon Lagergren <Lwonla@users.noreply.github.com>
    Co-authored-by: Natanael Augustin <NatanaelAugustin@users.noreply.github.com>
    Co-authored-by: andreaskostet <andreaskostet@users.noreply.github.com>

---
+++ manero-frontend/src/components/DiscountCard.jsx
@@ -0,0 +1,12 @@
+function DiscountCard() {
+  return (
+    <div className="discount-card">
+      <div className="content">
+        <h2>Take 50% off!</h2>
+        <button className="button-white">shop now</button>
+      </div>
+    </div>
+  );
+}
+
+export default DiscountCard;
