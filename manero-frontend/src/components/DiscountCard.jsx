55b242072668e5d299b8adf806f47167ad4c6180
simonoff <hellosimonoff@gmail.com>
2023-10-20 16:57:39 +0200

    Remove global styling for button element
    The element styling in _button.scss broke some things on the cart page

--- manero-frontend/src/components/DiscountCard.jsx
+++ manero-frontend/src/components/DiscountCard.jsx
@@ -3,7 +3,7 @@ function DiscountCard() {
     <div className="discount-card">
       <div className="content">
         <h2>Take 50% off!</h2>
-        <button className="button-white">shop now</button>
+        <button className="button button-white">shop now</button>
       </div>
     </div>
   );
