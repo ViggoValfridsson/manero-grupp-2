150692237d510fb5c96118d7ec621f8d7a095158
simonoff <hellosimonoff@gmail.com>
2023-10-20 16:59:43 +0200

    Fix handleAddToCart function and CartProductCard

--- manero-frontend/src/components/CartProductCard.jsx
+++ manero-frontend/src/components/CartProductCard.jsx
@@ -1,22 +1,29 @@
 import { Minus, Plus } from "lucide-react";
 import { apiDomain } from "../helpers/api-domain";
+import { Link } from "react-router-dom";
+import { handleAddToCart, handleRemoveFromCart } from "../helpers/add-to-cart";
+import { useContext } from "react";
+import { CartContext } from "../App";
 
 export function CartProductCard({ cartItem }) {
+  const cartState = useContext(CartContext);
+
   return (
     <div className="cart-product-card">
-      <img src={apiDomain.https + cartItem.imagePaths[0]} alt="" />
-      <div className="info">
-        <p>{cartItem.name}</p>
-        <span>${cartItem.price}</span>
-        <span>Size: {cartItem.size}</span>
-      </div>
+      <Link to={`/products/${cartItem.id}`}>
+        <img src={apiDomain.https + cartItem.imagePaths[0]} alt="" />
+        <div className="info">
+          <p>{cartItem.name}</p>
+          <span>${cartItem.price}</span>
+          <span>Size: {cartItem.size}</span>
+        </div>
+      </Link>
       <div className="buttons">
-        <button>
+        <button onClick={() => handleAddToCart(cartState, cartItem, 1, cartItem.size)}>
           <Plus size={16} />
         </button>
-        {/* TODO: Fix this */}
-        <span>{cartItem.amount || 1}</span>
-        <button>
+        <span>{cartItem.amount}</span>
+        <button onClick={() => handleRemoveFromCart(cartState, cartItem)}>
           <Minus size={16} />
         </button>
       </div>
--- manero-frontend/src/helpers/add-to-cart.js
+++ manero-frontend/src/helpers/add-to-cart.js
@@ -1,12 +1,32 @@
-export const handleAddToCart = (cartState, product, amount = 1, size = "M") => {
+const sameSizeAndId = (a, b) => a.id === b.id && a.size === b.size;
+
+export const handleAddToCart = (cartState, incomingProduct, amount = 1, size = "M") => {
   const [cart, setCart] = cartState;
 
-  const itemInCart = cart.find((item) => item.id === product.id);
+  // Add amount and size to the incoming product object
+  const newCartItem = { ...incomingProduct, amount, size };
 
-  if (itemInCart) {
-    amount += itemInCart.amount;
-    setCart([...cart.filter((item) => item.id !== itemInCart.id), { ...product, amount, size }]);
+  // Check if the incoming item is already in cart
+  const sameItemInCart = cart.find((item) => sameSizeAndId(item, newCartItem));
+
+  // Add to the amount if the item is already in cart else add new incoming item
+  if (sameItemInCart) {
+    sameItemInCart.amount += amount;
+    setCart([...cart]);
   } else {
-    setCart([...cart, { ...product, amount, size }]);
+    setCart([...cart, newCartItem]);
+  }
+};
+
+export const handleRemoveFromCart = (cartState, cartItem, amount = 1) => {
+  const [cart, setCart] = cartState;
+
+  // Lower amount if more than 0 else remove item from cart
+  // Spreading cart to force react to re-render
+  cartItem.amount -= amount;
+  if (cartItem.amount <= 0) {
+    setCart([...cart.filter((x) => !sameSizeAndId(x, cartItem))]);
+  } else {
+    setCart([...cart]);
   }
 };
