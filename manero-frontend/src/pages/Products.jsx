7443265972860e70ab8cac067535e9696a38db90
fenetreVRT <fenetreverte@outlook.com>
2023-10-24 14:00:21 +0200

    Initial product page styling
    Co-authored-by: Viggo Valfridsson <ViggoValfridsson@users.noreply.github.com>
    Co-authored-by: Simon <simon-off@users.noreply.github.com>
    Co-authored-by: nabi <MohammadNabi01@users.noreply.github.com>

--- manero-frontend/src/pages/Products.jsx
+++ manero-frontend/src/pages/Products.jsx
@@ -1,15 +1,35 @@
+import { useState } from "react";
 import ProductGridCard from "../components/ProductGridCard";
 import { apiDomain } from "../helpers/api-domain";
 import useFetch from "../hooks/useFetch";
 import useQuery from "../hooks/useQuery";
+import { SlidersHorizontal } from "lucide-react";
 
 function Products() {
   const query = useQuery();
   const products = useFetch(`${apiDomain.https}/api/products?${query.toString()}`);
+  const [sortBy, setSortBy] = useState("");
+
+  const handleChange = (event) => {
+    setSortBy(event.target.value);
+  };
 
   return (
     <div className="products-page">
-      <div className="filter-container"></div>
+      <div className="filter-container">
+        <button>
+          <SlidersHorizontal />
+          Filters
+        </button>
+        {/* To Do Add functionality */}
+        <select value={sortBy} onChange={handleChange}>
+          <option value="" disabled hidden>
+            Order By
+          </option>
+          <option value="lowest">Lowest Price</option>
+          <option value="highest">Highest Price</option>
+        </select>
+      </div>
       <div className="product-card-container">
         {products.data?.map((product) => (
           <ProductGridCard key={product.id} product={product} />
