2aa0d5ebf8e36edafa0ef32c140e906dd5f3d27b
Leon Lagergren <leon.lagergren@utb.ecutbildning.se>
2023-10-24 11:01:16 +0200

    Add test for productService.GetById
    Co-authored-by: Simon <simon-off@users.noreply.github.com>

--- WebAPI/WebAPI/Interface/Services/IProductService.cs
+++ WebAPI/WebAPI/Interface/Services/IProductService.cs
@@ -6,7 +6,7 @@ namespace WebAPI.Interface.Services;
 
 public interface IProductService
 {
-    Task<ProductDto?> GetById(int id);
+    Task<ProductDto?> GetByIdAsync(int id);
     Task<List<ProductDto>> GetAllAsync();
     Task<List<ProductDto>> GetAllFilteredAsync(List<Expression<Func<ProductEntity, bool>>> predicates);
 }
