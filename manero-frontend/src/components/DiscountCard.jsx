7e2959b5700a0c6899f85203693ededff11c7342
Natanael Augustin <natanael.augustin@gmail.com>
2023-10-25 13:28:51 +0200

    add a Link so user is redirect to products page

--- manero-frontend/src/components/DiscountCard.jsx
+++ manero-frontend/src/components/DiscountCard.jsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
+
 function DiscountCard() {
   return (
     <div className="discount-card">
       <div className="content">
         <h2>Take 50% off!</h2>
-        <button className="button button-white">shop now</button>
+        <Link to="/products">
+          <button className="button button-white">shop now</button>
+        </Link>
       </div>
     </div>
   );
