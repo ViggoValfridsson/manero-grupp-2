f09aa6e345dddfc5caae9fa5cd357d39b58fd4f5
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2023-10-25 10:10:47 +0200

    Add logic to show which tag and category is currently active

--- manero-frontend/src/components/Layout/FilterBurgerMenu.jsx
+++ manero-frontend/src/components/Layout/FilterBurgerMenu.jsx
@@ -1,4 +1,4 @@
-import { SlidersHorizontal } from "lucide-react";
+import { SlidersHorizontal, Check } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import useFetch from "../../hooks/useFetch";
 import { apiDomain } from "../../helpers/api-domain";
@@ -36,40 +36,50 @@ function FilterBurgerMenu({ filterMenuOpenState }) {
     navigate(`?${queries.toString()}`);
   };
 
-  console.log(categories.data);
+  const isCurrentQuery = (queryName, queryValue) => {
+    const currentUrl = new URL(window.location.href);
+    // Make everything lowercase otherwise queries.has() won't match
+    let queries = new URLSearchParams(currentUrl.search.toLowerCase());
+    queryName = queryName.toLowerCase();
+    queryValue = queryValue.toLowerCase();
+
+    if (queries.has(queryName, queryValue)) {
+      return true;
+    }
+
+    return false;
+  };
 
   return (
     <>
-      <button onClick={handleBurgerClick} className="contact-burger-button">
+      <button onClick={handleBurgerClick} className="filter-burger-button">
         <SlidersHorizontal />
         Filters
       </button>
       <div
         onClick={handleBackgroundClick}
-        className={`${sidebarOpen ? "open" : "closed"} contact-bg`}
+        className={`${sidebarOpen ? "open" : "closed"} filter-bg`}
       ></div>
-      <div className={`${sidebarOpen ? "open" : "closed"} contact-sidebar`}>
+      <div className={`${sidebarOpen ? "open" : "closed"} filter-sidebar`}>
         <div className="container">
-          <div className="content-top">
-            <div className="category-container">
-              <h2>Categories</h2>
-              {categories.data?.map((category) => (
-                <button
-                  key={category.name}
-                  onClick={() => handleQueryClick("categoryName", category.name)}
-                >
-                  {category.name}
-                </button>
-              ))}
-            </div>
-            <div className="tag-container">
-              <h2>Tags</h2>
-              {tags.data?.map((tag) => (
-                <button key={tag.name} onClick={() => handleQueryClick("tagName", tag.name)}>
-                  {tag.name}
-                </button>
-              ))}
-            </div>
+          <div className="category-container">
+            <h2>Categories</h2>
+            {categories.data?.map((category) => (
+              <button
+                key={category.name}
+                onClick={() => handleQueryClick("categoryName", category.name)}
+              >
+                {category.name} {isCurrentQuery("categoryName", category.name) && <Check />}
+              </button>
+            ))}
+          </div>
+          <div className="tag-container">
+            <h2>Tags</h2>
+            {tags.data?.map((tag) => (
+              <button key={tag.name} onClick={() => handleQueryClick("tagName", tag.name)}>
+                {tag.name} {isCurrentQuery("tagName", tag.name) && <Check />}
+              </button>
+            ))}
           </div>
         </div>
       </div>
