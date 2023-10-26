5a8664777bc97afdeff4bf4a19c3267c3e0666cb
simonoff <hellosimonoff@gmail.com>
2023-10-26 15:59:26 +0200

    Update checkout and order confirmation pages
    Co-authored-by: Jimmieost <Jimmieost@users.noreply.github.com>

---
+++ manero-frontend/src/helpers/hide-card-number.js
@@ -0,0 +1,14 @@
+export default function hideCardNumber(cardNumber) {
+  return cardNumber
+    .split("")
+    .map((char, i) => {
+      if (char === " ") {
+        return " ";
+      } else if (i >= cardNumber.length - 4) {
+        return char;
+      } else {
+        return "*";
+      }
+    })
+    .join("");
+}
