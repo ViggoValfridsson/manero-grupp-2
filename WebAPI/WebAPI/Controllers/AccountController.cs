f8b8946035eae129c8bd509344013f5552e77238
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2023-10-30 14:06:32 +0100

    Create sign in method

--- WebAPI/WebAPI/Controllers/AccountController.cs
+++ WebAPI/WebAPI/Controllers/AccountController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using WebAPI.Models.Dtos;
 using WebAPI.Models.Identity;
 using WebAPI.Models.Schemas;
@@ -12,14 +16,18 @@ namespace WebAPI.Controllers;
 public class AccountController : ControllerBase
 {
     private readonly UserManager<AppUser> _userManager;
+    private readonly SignInManager<AppUser> _signInManager;
+    private readonly IConfiguration _configuration;
 
-    public AccountController(SignInManager<AppUser> signInManager, UserManager<AppUser> userManager)
+    public AccountController(SignInManager<AppUser> signInManager, UserManager<AppUser> userManager, IConfiguration configuration)
     {
         _userManager = userManager;
+        _signInManager = signInManager;
+        _configuration = configuration;
     }
 
 
-    [HttpPost]
+    [HttpPost("SignUp")]
     public async Task<IActionResult> SignUp(SignUpSchema schema)
     {
         try
@@ -41,6 +49,46 @@ public class AccountController : ControllerBase
         catch
         {
             return StatusCode(502, "Something went wrong when signing up. Please try again.");
+        }
+    }
+
+    [HttpPost("SignIn")]
+    public async Task<IActionResult> SignIn(SignInSchema schema)
+    {
+        try
+        {
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if ((await _signInManager.PasswordSignInAsync(schema.Email, schema.Password, false, false)).Succeeded)
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.ASCII.GetBytes(_configuration["JwtSettings:Key"]!);
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new[]
+                    {
+                        new Claim(ClaimTypes.Name, schema.Email),
+                        // Add more claims here as needed
+                    }),
+                    Expires = DateTime.UtcNow.AddMonths(8),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+                    Issuer = "https://manero.com",
+                    Audience = "https://manero.com"
+                };
+
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+                var tokenString = tokenHandler.WriteToken(token);
+
+                return Ok(new { Token = tokenString });
+            }
+
+            return Unauthorized("Invalid login credentials, please try again.");
+        }
+        catch
+        {
+            return StatusCode(502, "Something went wrong when signing in. Please try again.");
         }
     }
 }
