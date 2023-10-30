8ba47c65dd1912617c77fac09ea4462b3c2f3f4a
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2023-10-30 11:33:40 +0100

    Create sign up schema

---
+++ WebAPI/WebAPI/Controllers/AccountController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AccountController : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> SignUp(SignUpSchema schema)
+}
---
+++ WebAPI/WebAPI/Models/Schemas/SignUpSchema.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Models.Schemas;
+
+public class SignUpSchema
+{
+    [Required]
+    [MaxLength(100)]
+    public string FirstName { get; set; } = null!;
+
+    [Required]
+    [MaxLength(100)]
+    public string LastName { get; set; } = null!;
+
+    [Required]
+    [MinLength(6)]
+    [MaxLength(320)]
+    [RegularExpression(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$")]
+    public string Email { get; set; } = null!;
+
+    [Required]
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$")]
+    public string Password { get; set; } = null!;
+
+    [Required]
+    [MinLength(10)]
+    [MaxLength(20)]
+    [RegularExpression(@"^(\+\d{1,4}\s?)?(\(?\d{1,}\)?[-.\s]?)+\d{1,}$")]
+    public required string PhoneNumber { get; set; }
+}
