fb4417b1921b1a940e9424e52dd7faf19f6d84bb
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2023-10-30 11:20:45 +0100

    Create new app user entity

---
+++ WebAPI/WebAPI/Models/Identity/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPI.Models.Identity;
+
+public class AppUser : IdentityUser
+{
+    [Column(TypeName = "nvarchar(100)")]
+    public string FirstName { get; set; } = null!;
+
+    [Column(TypeName = "nvarchar(100)")]
+    public string LastName { get; set; } = null!;
+}
