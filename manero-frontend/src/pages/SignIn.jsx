b32469bb853c7ccc4724e5919d41e072610810cf
Leon Lagergren <leon.lagergren@utb.ecutbildning.se>
2023-11-01 13:06:51 +0100

    Update css

--- manero-frontend/src/pages/SignIn.jsx
+++ manero-frontend/src/pages/SignIn.jsx
@@ -1,39 +1,49 @@
-import { ArrowRight, ChevronLeft } from "lucide-react";
+import { ChevronLeft, Check, EyeOff } from "lucide-react";
+import { Link } from "react-router-dom";
 export default function SignIn() {
   return (
+    <>
+      <div className="signin-container">
+        <ChevronLeft />
+        <p>SignIn</p>
+        <h1>Welcome Back!</h1>
+        <p>Sign in to continue</p>
 
-    <div className="signin">
-    <ChevronLeft />
-      <p>SignIn</p>
-      <h1>Welcome Back!</h1>
-      <p>Sign in to continue</p>
-      <div className="content-bottom">
-        <div className="input-container">
-          <input type="text" placeholder="kristinwatson@mail.com" />
-          <button type="submit">
-            <div className="container-arrow">
-              <ArrowRight />
-            </div>
-          </button>
+        <div className="signin-detail">
+
+          <div className="input-container">
+            <input type="text" placeholder="EMAIL" />
+            <button type="submit">
+              <div className="container-icon">
+                <Check />
+              </div>
+            </button>
+          </div>
+
+          <div className="input-container">
+            <input type="text" placeholder="PASSWORD" />
+            <button type="submit">
+              <div className="container-icon">
+                <EyeOff />
+              </div>
+            </button>
+          </div>
+          
         </div>
-        <div className="input-container">
-          <input type="text" placeholder="Password" />
-          <button type="submit">
-            <div className="container-arrow">
-              <ArrowRight />
-            </div>
-          </button>
+
+        <form>
+          <input type="checkbox" id="rememberme" name="rememberme" value="check" />
+          <label for="rememberme"> Remember me</label>
+        </form>
+        <p>Forgot password?</p>
+
+        <div className="signin-button-wrapper">
+          <Link to="/signin" className="button button-black">
+            SIGN IN
+          </Link>
         </div>
+        <p>Don’t have an account? Sign up.</p>
       </div>
-      <form>
-        <input type="checkbox" id="rememberme" name="rememberme" value="check" />
-        <label for="rememberme"> Remember me</label>
-      </form>
-      <p>Forgot password?</p>
-      
-      <button className="button button-black" type="button">SIGN IN</button>
-      <p>Don’t have an account? Sign up.</p>
-    </div>
-
+    </>
   );
 }
