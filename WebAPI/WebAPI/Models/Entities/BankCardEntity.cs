654d4e57c60dee18b542f8428f3e7c547cde9b4b
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2023-11-01 11:34:11 +0100

    Create entity and migration
    Co-authored-by: andreaskostet <andreaskostet@users.noreply.github.com>
    Co-authored-by: Jimmieost <Jimmieost@users.noreply.github.com>

---
+++ WebAPI/WebAPI/Models/Entities/BankCardEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPI.Models.Entities;
+
+public class BankCardEntity
+{
+    public int Id { get; set; }
+
+    [Column(TypeName = "nvarchar(16)")]
+    public required string CreditCardNumber { get; set; }
+    public int CVC { get; set; }
+    public required string CardholderName { get; set; }
+    public required string CardIssuer { get; set; }
+}
