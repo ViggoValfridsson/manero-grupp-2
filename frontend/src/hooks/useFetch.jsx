e56d527edac32611432ede2081fec382f3c470c6
Jimmieost <jimmie.ost@utb.ecutbildning.se>
2023-11-06 13:45:14 +0100

    created profile page logic

--- frontend/src/hooks/useFetch.jsx
+++ frontend/src/hooks/useFetch.jsx
@@ -1,15 +1,27 @@
 import { useEffect, useState } from "react";
+import getCookieByName from "../helpers/getCookieByName";
 
 export default function useFetch(url) {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const authToken = getCookieByName("Authorization");
+
+  const headers = new Headers();
+  headers.append("Content-Type", "application/json");
+
+  if (authToken) {
+    headers.append("Authorization", `Bearer ${authToken}`);
+  }
 
   useEffect(() => {
     const getData = async () => {
       try {
-        setIsLoading(true);
-        const response = await fetch(url);
+        const response = await fetch(url, {
+          method: "GET",
+          headers: headers,
+        });
+
         if (!response.ok) {
           throw Error(`${response.status}: ${await response.text()}`);
         }
