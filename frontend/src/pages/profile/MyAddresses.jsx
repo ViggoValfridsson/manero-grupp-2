a0dfb04b64f6dba32dbd92028d1f608a0c8dccfb
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2023-11-07 14:36:10 +0100

    Style main address page
    Co-authored-by: Natanael Augustin <NatanaelAugustin@users.noreply.github.com>

--- frontend/src/pages/profile/MyAddresses.jsx
+++ frontend/src/pages/profile/MyAddresses.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { apiDomain } from "../../helpers/api-domain";
 import useFetch from "../../hooks/useFetch";
+import { MapPin, PencilLine, Plus } from "lucide-react";
 
 function MyAddresses() {
   const addresses = useFetch(`${apiDomain.https}/api/addresses`);
@@ -9,7 +10,31 @@ function MyAddresses() {
 
   return (
     <div className="my-addresses-page">
-      <Link to={"/profile/address/add"}>Add</Link>
+      <div className="heading">
+        <h1>Add Address</h1>
+      </div>
+      <ul>
+        {addresses?.data?.map((address) => (
+          <li key={address.id}>
+            <Link to={`/profile/address/${address.id}`}>
+              <MapPin />
+              <div className="text-container">
+                <h2>{address.streetName}</h2>
+                <p>
+                  {address.city}, {address.postalCode}
+                </p>
+              </div>
+              <PencilLine />
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <div className="add-container">
+        <Link to="/profile/address/add">
+          <Plus />
+          <span>Add new address</span>
+        </Link>
+      </div>
     </div>
   );
 }
