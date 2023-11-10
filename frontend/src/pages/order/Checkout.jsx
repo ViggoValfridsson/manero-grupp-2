ba7b76b9b0e17efd35402d7da912dc071575aef0
simonoff <hellosimonoff@gmail.com>
2023-11-10 12:41:17 +0100

    Use saved info on checkout
    Co-authored-by: Erik Östemark <fenetreVRT@users.noreply.github.com>
    Co-authored-by: Jimmieost <Jimmieost@users.noreply.github.com>
    Co-authored-by: Andreas Kostet <andreaskostet@users.noreply.github.com>
    Co-authored-by: Natanael Augustin <NatanaelAugustin@users.noreply.github.com>

--- frontend/src/pages/order/Checkout.jsx
+++ frontend/src/pages/order/Checkout.jsx
@@ -4,7 +4,7 @@ import { useCart } from "../../hooks/useCart";
 import { ChevronRight } from "lucide-react";
 import CheckoutItem from "../../components/CheckoutItem";
 import { useOrder } from "../../hooks/useOrder";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import hideCreditCardNumber from "../../helpers/hideCreditCardNumber";
 
 function Checkout() {
@@ -13,6 +13,13 @@ function Checkout() {
   const navigate = useNavigate();
   const [shippingError, setShippingError] = useState(false);
   const [paymentError, setPaymentError] = useState(false);
+
+  useEffect(() => {
+    if (cart.length <= 0) {
+      navigate("/cart");
+      return;
+    }
+  }, [cart, navigate]);
 
   const handlePlaceOrder = () => {
     setShippingError(!address && !customer);
@@ -46,6 +53,7 @@ function Checkout() {
         <Link to="/checkout/shipping" className={shippingError ? "error" : ""}>
           <div className="info">
             <h2>Shipping Details {shippingError && "*"}</h2>
+            <p>{customer && `${customer?.firstName} ${customer?.lastName}`}</p>
             <p>{address && `${address?.streetAddress}, ${address?.postalCode} ${address?.city}`}</p>
           </div>
           <div className="icon">
@@ -65,10 +73,10 @@ function Checkout() {
       <section className="container-bottom">
         <div className="comment-field">
           <label>Comment</label>
-          <input
+          <textarea
             placeholder="Write your comment"
             onChange={(e) => setOrderComment(e.target.value)}
-            value={orderComment}
+            value={orderComment ?? ""}
           />
         </div>
         <div className="order-button">
